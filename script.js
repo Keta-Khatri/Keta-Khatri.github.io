9256da2b4aa02bac22cc11146645fc4bdda77422
Keta <k3khatri@uwaterloo.ca>
2022-09-11 21:46:11 -0400

    updated color

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Engineer"],
+        strings: ["Software Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
